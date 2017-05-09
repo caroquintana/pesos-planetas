66e6769df03a54fa74f4b3c146ed0afa54a37a72
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-08 22:13:02 -0300

    version funcional

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+var contenedor = document.getElementById("contenedor");
+
+function calcular(){
+	var masa = document.getElementById("peso").value;
+	var gravedad = document.getElementById("planetas").value;
+	return alert(masa * gravedad)
+}
