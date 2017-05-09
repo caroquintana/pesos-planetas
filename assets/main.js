ea8c2d8d5a7defaf6839ee245442a1f031df5dca
maitc <maibeet.torres@gmail.com>
2017-05-08 20:12:37 -0400

    funcionalidad ok

---
+++ assets/main.js
@@ -0,0 +1,33 @@
+
+	function venus(){
+		var peso = document.getElementById("vens").value;
+		var res = (peso * 0.87).toFixed(2);
+		return alert("Tu peso en Venus es " + res);
+	}
+	function marte(){
+		var peso = document.getElementById("mart").value;
+		var res = (peso * 0.38).toFixed(2);
+		return alert("Tu peso en Marte es " + res);
+	}
+	function jupiter(){
+		var peso = document.getElementById("jupi").value;
+		var res = (peso * 2.55).toFixed(2);
+		return alert("Tu peso en Jupiter es " + res);
+	}
+	function saturno(){
+		var peso = document.getElementById("satur").value;
+		var res = (peso * 0.93).toFixed(2);
+		return alert("Tu peso en Saturno es " + res);
+	}
+	function urano(){
+		var peso = document.getElementById("uran").value;
+		var res = (peso * 0.99).toFixed(2);
+		return alert("Tu peso en Urano es " + res);
+	}
+	function neptuno(){
+		var peso = document.getElementById("neptu").value;
+		var res = (peso * 1.38).toFixed(2);
+		return alert("Tu peso en Neptuno es " + res);
+	}
+
+	
